ce7e8f0f452ad6f96823f3f5322f21734873bbf5
Cristian031 <magallanesefidani@gmail.com>
2023-09-10 18:53:36 -0300

    Se agrega el Proyecto del TP1

---
+++ src/main/java/com/pesos/TP1JPA/Tp1JpaApplication.java
@@ -0,0 +1,14 @@
+package com.pesos.TP1JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Tp1JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1JpaApplication.class, args);
+	}
+
+}
